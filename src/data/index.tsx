7818ac68e214b670008b8d59cb1a11c0cadf9a1a
Personal Profile <davina.leong.sy@gmail.com>
2023-04-25 20:27:56 +0800

    Added data index

---
+++ src/data/index.tsx
@@ -0,0 +1,17 @@
+import CompletedMessagesData from './completed-messages.data'
+import ErrorsData from './errors.data'
+import OptionsData from './options.data'
+import QuizzesData from './quizzes.data'
+import StatusesData from './statuses.data'
+import StepsData from './steps.data'
+
+const Data = {
+  CompletedMessagesData,
+  ErrorsData,
+  OptionsData,
+  QuizzesData,
+  StatusesData,
+  StepsData,
+}
+
+export default Data
